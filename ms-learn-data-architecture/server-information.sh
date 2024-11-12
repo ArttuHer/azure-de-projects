2c6ef29489dab7fed339c0c170d0addc8afe766c
ArttuHer <arttu.heroja@gmail.com>
2024-11-12 15:13:08 +0200

    added usefull commands how to connect database

--- ms-learn-data-architecture/server-information.sh
+++ ms-learn-data-architecture/server-information.sh
@@ -1,3 +1,5 @@
 az configure --defaults group=ms-learn-rg sql-server=logisticss-db-dev
 az sql db list
 az sql db list | jq '[.[] | {name: .name}]'
+sqlcmd -S tcp:logisticss-db-dev.database.windows.net,1433 -d Logistics -U <username> -P <password> -N -l 30
+group delete --name <resourcegroup>
