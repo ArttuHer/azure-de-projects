d500769ff7735dfe9f2e16be10069f080df14026
ArttuHer <arttu.heroja@gmail.com>
2024-11-13 09:01:54 +0200

    removed unused commands

--- ms-learn-data-architecture/az-create-server.sh
+++ ms-learn-data-architecture/az-create-server.sh
@@ -1,1 +1,19 @@
-az group list --output table
+iaz group list --output table
+
+SERVERNAME="fitnesssqlserver-1998"
+
+# Define the resource group name (ensure it exists or create it beforehand)
+RESOURCE_GROUP="mslearn-rg"
+
+# Define the location (e.g., eastus, westus, etc.)
+LOCATION="swedencentral"
+
+# Define the SQL server admin username
+ADMIN_LOGIN="ServerAdmin"
+
+# Define the SQL server admin password (must meet Azure's complexity requirements)
+PASSWORD="Tero1000"
+
+az sql server create --name $SERVERNAME --resource-group $RESOURCE_GROUP --location $LOCATION --admin-user $ADMIN_LOGIN --admin-password $PASSWORD
+az sql server list --resource-group  --output table
+
