59ca54a7ec26b8c3a74132ad3686a81cbad3fd59
ArttuHer <arttu.heroja@gmail.com>
2024-11-14 14:59:53 +0200

    changed name

--- terraform-azure-project/main.tf
+++ terraform-azure-project/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "mtc-rg" {
   }
 }
 
-resource "azurerm_virtual_network" "mtc-vm" {
+resource "azurerm_virtual_network" "mtc-vn" {
   name                = "mtc-network"
   resource_group_name = azurerm_resource_group.mtc-rg.name
   location            = azurerm_resource_group.mtc-rg.location
@@ -32,7 +32,7 @@ resource "azurerm_virtual_network" "mtc-vm" {
 resource "azurerm_subnet" "mtc-subnet" {
   name                 = "mtc-subnet"
   resource_group_name  = azurerm_resource_group.mtc-rg.name
-  virtual_network_name = azurerm_virtual_network.mtc-vm.name
+  virtual_network_name = azurerm_virtual_network.mtc-vn.name
   address_prefixes     = ["10.123.1.0/24"]
 }
 
