ea8d4dea048b63273b8ffcd7a238ff8b43f20be6
ArttuHer <arttu.heroja@gmail.com>
2024-11-14 09:26:56 +0200

    typo fixes

--- terraform-azure-project/main.tf
+++ terraform-azure-project/main.tf
@@ -20,19 +20,18 @@ resource "azurerm_resource_group" "mtc-rg" {
 }
 
 resource "azurerm_virtual_network" "mtc-vm" {
-  name = "mtc-network"
+  name                = "mtc-network"
   resource_group_name = azurerm_resource_group.mtc-rg.name
-  location = azurerm_resource_group.mtc-rg.location
-  address_space = ["10.123.0.0/16"]
+  location            = azurerm_resource_group.mtc-rg.location
+  address_space       = ["10.123.0.0/16"]
   tags = {
     environment = "dev"
   }
 }
 
 resource "azurerm_subnet" "mtc-subnet" {
-  name = "mtc-subnet-1"
-  resource_group_name = azurerm_resource_group.mtc-rg.name
-  location = azurerm_resource_group.mtc-rg.location
-  virtual_network_name = azurerm_resource_group.mtc-vm.name
-  address_prefixes = ["10.0.1.0/24"]
+  name                 = "mtc-subnet-1"
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
+  virtual_network_name = azurerm_virtual_network.mtc-vm.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
