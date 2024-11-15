b5b939fd0bdbe20b1f81311742a3174fe029e6a5
ArttuHer <arttu.heroja@gmail.com>
2024-11-15 13:34:29 +0200

    environmental variable

--- terraform-azure-data-project/variables.tf
+++ terraform-azure-data-project/variables.tf
@@ -1,5 +1,5 @@
 variable "environment" {
   type        = string
-  default     = "dev"  # Optional: Default value
+  default     = "dev" # Optional: Default value
   description = "The target environment (e.g., dev, test, prod)"
 }
