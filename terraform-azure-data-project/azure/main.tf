608384355cabd14ab12d031668eeb95ca3e9606c
ArttuHer <arttu.heroja@gmail.com>
2024-11-18 10:10:35 +0200

    output rg and location

--- terraform-azure-data-project/azure/main.tf
+++ terraform-azure-data-project/azure/main.tf
@@ -12,6 +12,14 @@ resource "azurerm_resource_group" "tf-data-rg" {
   }
 }
 
+output "resource_group_name" {
+  value = azurerm_resource_group.tf-data-rg.name
+}
+
+output "location" {
+  value = azurerm_resource_group.tf-data-rg.location
+}
+
 resource "azurerm_storage_account" "tf-sa" {
   name                     = "sms${var.environment}swedencentralsa001"
   resource_group_name      = azurerm_resource_group.tf-data-rg.name
