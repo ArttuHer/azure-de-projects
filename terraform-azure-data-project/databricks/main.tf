9fd6c5449c341021c6c8ece2cab9236e42dafe4e
ArttuHer <arttu.heroja@gmail.com>
2024-11-18 10:21:21 +0200

    moved configuration to azure/main

--- terraform-azure-data-project/databricks/main.tf
+++ terraform-azure-data-project/databricks/main.tf
@@ -12,14 +12,3 @@ variable "environment" {
   type        = string
   description = "Default value for target environment"
 }
-
-resource "azurerm_databricks_workspace" "databricks-ws" {
-  name                = "databricks-ws-${var.environment}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku                 = "standard"
-
-  tags = {
-    environment = "${var.environment}"
-  }
-}
