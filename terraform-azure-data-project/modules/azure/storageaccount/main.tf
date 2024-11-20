75ec579fc2139e4ce78f02aa6780bca04632c85f
ArttuHer <arttu.heroja@gmail.com>
2024-11-20 12:24:19 +0200

    use variables passed from other module

--- terraform-azure-data-project/modules/azure/storageaccount/main.tf
+++ terraform-azure-data-project/modules/azure/storageaccount/main.tf
@@ -1,7 +1,22 @@
+variable "environment" {
+  type        = string
+  description = "Default value for target environment"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "resource_group_location" {
+  description = "Location of the resource group"
+  type        = string
+}
+
 resource "azurerm_storage_account" "tf-sa" {
   name                     = "sms${var.environment}swedencentralsa001"
-  resource_group_name      = azurerm_resource_group.tf-data-rg.name
-  location                 = azurerm_resource_group.tf-data-rg.location
+  resource_group_name      = var.resource_group_name
+  location                 = var.resource_group_location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
