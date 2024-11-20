cbb90fd5cfea86ab5fc3a70610a867d3fbe08adc
ArttuHer <arttu.heroja@gmail.com>
2024-11-20 12:42:13 +0200

    databricks ws

---
+++ terraform-azure-data-project/modules/azure/databricksws/main.tf
@@ -0,0 +1,25 @@
+variable "environment" {
+  type        = string
+  description = "Default value for target environment"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "resource_group_location" {
+  description = "Location of the resource group"
+  type        = string
+}
+
+resource "azurerm_databricks_workspace" "databricks-ws" {
+  name                = "${var.environment}-databricks-ws"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  sku                 = "standard"
+
+  tags = {
+    environment = "${var.environment}"
+  }
+}
