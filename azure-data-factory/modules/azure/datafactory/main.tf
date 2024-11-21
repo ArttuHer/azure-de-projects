cb0044f2a7ef2a81bc02a83d6441687d5a1d6071
ArttuHer <arttu.heroja@gmail.com>
2024-11-21 14:37:19 +0200

    output data factory id

--- azure-data-factory/modules/azure/datafactory/main.tf
+++ azure-data-factory/modules/azure/datafactory/main.tf
@@ -13,3 +13,7 @@ resource "azurerm_data_factory" "az-df" {
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
 }
+
+output "data_factory_id" {
+  value = azurerm_data_factory.az-df.id
+}
