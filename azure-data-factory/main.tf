ec38d47572c36493af0e11f419f33e9a6be3120c
ArttuHer <arttu.heroja@gmail.com>
2024-11-21 14:21:07 +0200

    data factory module

--- azure-data-factory/main.tf
+++ azure-data-factory/main.tf
@@ -12,5 +12,11 @@ provider "azurerm" {
 }
 
 module "azure_resource_group" {
-  source      = "./modules/azure/resourcegroup"
+  source = "./modules/azure/resourcegroup"
+}
+
+module "azure_data_factory" {
+  source                  = "./modules/azure/datafactory"
+  resource_group_name     = module.azure_resource_group.resource_group_name
+  resource_group_location = module.azure_resource_group.location
 }
