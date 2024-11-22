c4bdce05c2a8499b7bcaa135e683c68b06d3a922
ArttuHer <arttu.heroja@gmail.com>
2024-11-22 09:23:04 +0200

    create databricks ws

--- azure-data-factory/main.tf
+++ azure-data-factory/main.tf
@@ -21,14 +21,14 @@ module "azure_data_factory" {
   resource_group_location = module.azure_resource_group.location
 }
 
-/* module "databricks_workspace" {
+module "databricks_workspace" {
   source                  = "./modules/azure/databricksws"
   environment             = var.environment
   resource_group_name     = module.azure_resource_group.resource_group_name
   resource_group_location = module.azure_resource_group.location
 }
 
-module "datafactory_linked_service" {
+/* module "datafactory_linked_service" {
   source          = "./modules/azure/dflinkedservices"
   data_factory_id = module.azure_data_factory.data_factory_id
   workspace_url   = module.databricks_workspace.workspace_url
