7591903c9c7c7deb1b45ba701b9ed5db2198a1bb
ArttuHer <arttu.heroja@gmail.com>
2024-11-22 08:04:10 +0200

    create linked service

--- azure-data-factory/modules/azure/dflinkedservices/main.tf
+++ azure-data-factory/modules/azure/dflinkedservices/main.tf
@@ -2,3 +2,15 @@ variable "data_factory_id" {
   description = "Data factory id"
   type        = string
 }
+
+variable "databricks_url" {
+  description = "Databricks workspace url"
+  type        = string
+}
+
+resource "azurerm_data_factory_linked_service_azure_databricks" "databricks" {
+  name            = "ADBLinkedServiceViaMSI"
+  data_factory_id = var.data_factory_id
+  description     = "ADB Linked Service via MSI"
+  adb_domain      = "https://${var.databricks_url}"
+}
