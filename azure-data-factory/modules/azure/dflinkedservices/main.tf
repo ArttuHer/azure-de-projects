b1d71fbe9e07a974ec34e4026bb4630efef5a771
ArttuHer <arttu.heroja@gmail.com>
2024-11-25 08:04:12 +0200

    use existin cluster

--- azure-data-factory/modules/azure/dflinkedservices/main.tf
+++ azure-data-factory/modules/azure/dflinkedservices/main.tf
@@ -14,20 +14,11 @@ variable "workspace_id" {
 }
 
 resource "azurerm_data_factory_linked_service_azure_databricks" "databricks" {
-  name            = "ADBLinkedServiceViaMSI"
-  data_factory_id = var.data_factory_id
-  description     = "ADB Linked Service via MSI"
-  adb_domain      = "https://${var.workspace_url}"
+  name                = "LS_databricks"
+  data_factory_id     = var.data_factory_id
+  description         = "ADB Linked Service via MSI"
+  existing_cluster_id = "1122-071514-jfedgut"
 
   msi_work_space_resource_id = var.workspace_id
-
-  new_cluster_config {
-    node_type             = "Standard_NC12"
-    cluster_version       = "5.5.x-gpu-scala2.11"
-    min_number_of_workers = 1
-    max_number_of_workers = 4
-    driver_node_type      = "Standard_NC12"
-    log_destination       = "dbfs:/logs"
-  }
-
+  adb_domain                 = "https://${var.workspace_url}"
 }
